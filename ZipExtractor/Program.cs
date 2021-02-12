d16e33ac0957191b676d829f92be15629e1e1d0b
BuildTools <unconfigured@null.spigotmc.org>
2021-02-12 11:39:40 +0900

    update commit

--- ZipExtractor/Program.cs
+++ ZipExtractor/Program.cs
@@ -5,6 +5,61 @@ namespace ZipExtractor
 {
     class Program
     {
+        // 圧縮処理
+        private static void Compress()
+        {
+            Console.Write("圧縮したいディレクトリを指定してください。");
+            var startPath = @Console.ReadLine();
+            string zipName = @"\compressed.zip";
+            string prevPath = @"\..";
+            string zipPath = @startPath + prevPath + zipName;
+            Console.Write("[確認]\n 出力先: " + zipPath + "\n よろしいですか？(y/n): ");
+            var compressConf = Console.ReadLine();
+            if (compressConf == "y")
+            {
+                ZipFile.CreateFromDirectory(
+                    startPath,
+                    zipPath,
+                    CompressionLevel.Optimal,
+                    false,
+                    System.Text.Encoding.GetEncoding("UTF-8"));
+                Console.Write("完了しました。");
+            }
+            else if (compressConf == "n")
+            {
+                Console.Write("キャンセルされました。");
+            }
+            else
+            {
+                Console.Write("エラー: yまたはnを入力してください。");
+            }
+        }
+
+        // 解凍処理
+        private static void Extract()
+        {
+            Console.Write("解凍したいzipファイルを指定してください。");
+            var zipPath = @Console.ReadLine();
+            Console.Write("解凍先のフォルダを指定してください。");
+            var extractPath = @Console.ReadLine();
+            Console.Write("[確認]\n 解凍先: " + extractPath + "\n 解凍するファイルのディレクトリ: " + zipPath + "\n よろしいですか？(y/n): ");
+            var extractConf = Console.ReadLine();
+            if (extractConf == "y")
+            {
+                ZipFile.ExtractToDirectory(
+                    zipPath,
+                    extractPath);
+            }
+            else if (extractConf == "n")
+            {
+                Console.Write("キャンセルされました。");
+            }
+            else
+            {
+                Console.Write("エラー: yまたはnを入力してください。");
+            }
+        }
+
         static void Main()
         {
             Console.Write("ZipExtractor [Version 0.0.1]\n");
@@ -12,65 +67,15 @@ namespace ZipExtractor
             var Process = Console.ReadLine();
             if (Process == "圧縮")
             {
-                // 圧縮処理
-                Console.Write("圧縮したいディレクトリを指定してください。");
-                var startPath = @Console.ReadLine();
-                string zipName = @"\compressed.zip";
-                string prevPath = @"\..";
-                string zipPath = @startPath + prevPath + zipName;
-                Console.Write("[確認]\n 出力先: " + zipPath + "\n よろしいですか？(y/n): ");
-                var compressConf = Console.ReadLine();
-                if (compressConf == "y")
-                {
-                    ZipFile.CreateFromDirectory(
-                        startPath,
-                        zipPath,
-                        CompressionLevel.Optimal,
-                        false,
-                        System.Text.Encoding.GetEncoding("UTF-8"));
-                }
-                else if (compressConf == "n")
-                {
-                    Console.Write("キャンセルされました。");
-                    return;
-                }
-                else
-                {
-                    Console.Write("エラー: yまたはnを入力してください。");
-                    return;
-                }
+                Compress();
             }
-
             else if (Process == "解凍")
             {
-                // 解凍処理
-                Console.Write("解凍したいzipファイルを指定してください。");
-                var zipPath = @Console.ReadLine();
-                Console.Write("解凍先のフォルダを指定してください。");
-                var extractPath = @Console.ReadLine();
-                Console.Write("[確認]\n 解凍先: " + extractPath + "\n 解凍するファイルのディレクトリ: " + zipPath + "\n よろしいですか？(y/n): ");
-                var extractConf = Console.ReadLine();
-                if (extractConf == "y")
-                {
-                    ZipFile.ExtractToDirectory(
-                        zipPath,
-                        extractPath);
-                }
-                else if (extractConf == "n")
-                {
-                    Console.Write("キャンセルされました。");
-                    return;
-                }
-                else
-                {
-                    Console.Write("エラー: yまたはnを入力してください。");
-                    return;
-                }
+                Extract();
             }
-
             else
             {
-                return;
+                Console.Write("エラー: 圧縮または解凍を入力してください。");
             }
         }
     }
